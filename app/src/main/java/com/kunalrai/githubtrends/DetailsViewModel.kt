627748a8b797cdcaeee932b310e84c4ad45f1d28
Kunal <kunalrai.cse16@nituk.ac.in>
2019-11-19 20:38:49 +0530

    fragments,viewmodel and navigation

---
+++ app/src/main/java/com/kunalrai/githubtrends/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.kunalrai.githubtrends
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/kunalrai/githubtrends/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.kunalrai.githubtrends
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
