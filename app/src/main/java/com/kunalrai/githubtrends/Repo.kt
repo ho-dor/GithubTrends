d0dd12d8469f9ec2ff15193577e7848e53455cf1
Kunal <kunalrai.cse16@nituk.ac.in>
2019-11-25 08:53:18 +0530

    added data class for moshi

--- app/src/main/java/com/kunalrai/githubtrends/Repo.kt
+++ app/src/main/java/com/kunalrai/githubtrends/Repo.kt
@@ -1,5 +1,22 @@
 package com.kunalrai.githubtrends
 
+import com.squareup.moshi.Json
+
 data class Repo(
-    var string: String
+    @Json(name = "author")
+    var author: String,
+    @Json(name = "name")
+    var name: String,
+    @Json(name = "description")
+    var desc: String,
+    @Json(name = "avatar")
+    var avatar: String,
+    @Json(name = "language")
+    var language: String,
+    @Json(name = "url")
+    var url: String,
+    @Json(name = "stars")
+    var stars: String,
+    @Json(name = "forks")
+    var forks: String
 )
