d9cc933239e4dce400335d5059ba4f779b073354
Kunal <kunalrai.cse16@nituk.ac.in>
2019-11-29 23:01:56 +0530

    repo list displayed

--- app/src/main/java/com/kunalrai/githubtrends/ListFragment.kt
+++ app/src/main/java/com/kunalrai/githubtrends/ListFragment.kt
@@ -2,6 +2,7 @@ package com.kunalrai.githubtrends
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,7 +26,20 @@ class ListFragment : Fragment() {
     private lateinit var binding: ListFragmentBinding
     var recyclerView: RecyclerView? = null
     lateinit var listAdapter: ListAdapter
-    var repoList: List<Repo>? = null
+    var repoList: List<Repo> = listOf()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        viewModel.getRepos().observe(this,
+//            Observer<List<Repo>> {
+//                it?.let { repoList ->
+//                    this.repoList = repoList
+//                    Log.i("inside observe",""+repoList)
+//                }
+//            })
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -33,10 +47,18 @@ class ListFragment : Fragment() {
         viewModel.getRepos().observe(this,
             Observer<List<Repo>> {
                 it?.let { repoList ->
+                    this.repoList = repoList
+                    Log.i("inside observe",""+repoList)
                     listAdapter = ListAdapter(context, repoList)
                     recyclerView?.adapter = listAdapter
                 }
             })
+
+        recyclerView = view.findViewById(R.id.rv_repo_list)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        recyclerView?.setHasFixedSize(true)
+
+
     }
 
     override fun onCreateView(
@@ -49,9 +71,11 @@ class ListFragment : Fragment() {
         binding.viewmodel = viewModel
         setHasOptionsMenu(true)
 
-        recyclerView = view?.findViewById(R.id.rv_repo_list)
-        recyclerView?.layoutManager = LinearLayoutManager(context)
-        recyclerView?.setHasFixedSize(true)
+//        recyclerView = view?.findViewById(R.id.rv_repo_list)
+//        recyclerView?.layoutManager = LinearLayoutManager(context)
+//        recyclerView?.setHasFixedSize(true)
+//        listAdapter = ListAdapter(context, repoList)
+//        recyclerView?.adapter = listAdapter
 
 
         return binding.root
