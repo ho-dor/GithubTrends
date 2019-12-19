951fa3ed9ee62e30356a729d05a2d023c35ff38b
Kunal <kunalrai.cse16@nituk.ac.in>
2019-12-19 11:55:04 +0530

    added swipe-to-refresh

--- app/src/main/java/com/kunalrai/githubtrends/ListFragment.kt
+++ app/src/main/java/com/kunalrai/githubtrends/ListFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.facebook.shimmer.ShimmerFrameLayout
 import com.kunalrai.githubtrends.databinding.ListFragmentBinding
 
@@ -50,17 +51,24 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.i("fragment","onviewcreated")
+        refreshData()
+        binding.swipeRefresh.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+            refreshData()
+        })
+    }
+
+
+    fun refreshData() {
         viewModel.getRepos().observe(this,
             Observer<List<Repo>> {
                 it?.let { repoList ->
                     Log.i("data",""+repoList.size)
                     listAdapter.swapData(repoList)
+                    binding.swipeRefresh.isRefreshing = false
                     binding.shimmerViewContainer.stopShimmer()
                     binding.shimmerViewContainer.visibility = View.GONE
                 }
             })
-        //binding.shimmerViewContainer.stopShimmer()
     }
 
     override fun onPause() {
